431235e6ef704d0151d8e398ba4730af8f800861
unknown <vladimir_ostrovski@web.de>
2024-06-02 20:29:13 +0200

    work on the normal example

--- example_normal_distribution.R
+++ example_normal_distribution.R
@@ -11,7 +11,7 @@ F<-function(x){
   pnorm(x,mean=0, sd=1)
 }
 
-# Version 1 : mean of variance of sample distribution are known to be 0 and 1 
+# Version 1 : mean of variance of sample distribution are known to be 0 and 1. 
 
 parameter=list() # a list named parameter, this will store parameters of the equivalence test
 parameter$F=F # theoretical distribution, G in the paper
@@ -22,7 +22,8 @@ n=100 # number of observations in sample
 parameter$x=rnorm(n) # generates a random sample of size n from standard normal distribution
 asymptoticTest(parameter)
 
-# Version 2. mean and variance of sample distribution is unknown
+# Version 2. mean and variance of sample distribution is unknown.
+
 # This version is not completely correct from a theoretical point of view,
 # because the normalization of the data sample introduces some dependence between observations. 
 # Therefore, the assumption of identically independent observations is no longer correct.  
@@ -46,4 +47,36 @@ parameter$x=x # data sample
 
 asymptoticTest(parameter)
 
+# Version 3. mean and variance of sample distribution is unknown.
 
+# We split the data into two subsamples. 
+# The mean and sd of the normal distribution are estimated using the first subsample. 
+# The estimated values can then be used to test the second part of the data against the normal distribution,
+# whose parameters are the estimated mean and standard deviation.
+# This version is perfectly correct from a theoretical point of view, because conditional on the estimated parameter, we are testing against a fully specified normal distribution.
+
+
+n=200 # number of observations in sample
+x=rnorm(n, -5, 10) # generates a random sample of size n from some not standard normal distribution
+
+# split the data
+x1=x[1:100]
+x2=x[101:200]
+
+# estimate the parameter using first subset
+mu=mean(x1)
+sigma=sd(x1)
+
+#normalize the second subset using mu and sigma
+
+x2=(x2-mu)/sigma
+mean(x2)
+sd(x2)
+
+parameter=list() # a list named parameter, this will store parameters of the equivalence test
+parameter$F=F # theoretical distribution, G in the paper
+parameter$alpha=0.05 # significance level of the test
+parameter$nSimulation=1000 # number of samples in case of bootstrap test
+parameter$x=x2 # data sample
+
+asymptoticTest(parameter)
